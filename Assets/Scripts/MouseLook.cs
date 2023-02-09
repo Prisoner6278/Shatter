f1daf8b0e3d5fb5e139fa61fa6b87d0f6f9006cb
Prisoner6278 <84802821+Prisoner6278@users.noreply.github.com>
2023-02-09 13:43:45 -0600

    Changes
    Limit player rotation, added slowmo script.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -9,6 +9,7 @@ public class MouseLook : MonoBehaviour
     public Transform playerBody;
 
     float xRotation = 0f;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,7 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        Debug.Log(mouseX + ":" + mouseY);
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
--- Assets/Scripts/Playermovements.cs
+++ Assets/Scripts/Playermovements.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class Playermovements : MonoBehaviour
 {
+    //For LimitRot()
+    public float maxYRot = 20f;
+    public float minYRot = -20f;
+    private Transform localTrans;
+
+    //For WASDMove()
     public float speed = 10f;
+
+    //For MoveForward()
     private float mySpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        localTrans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveForward();
+        WASDMove();
+        LimitRot();
+
+
+    }
+
+    void MoveForward()
+    {
+        gameObject.transform.Translate(Vector3.forward * mySpeed * Time.deltaTime, Space.World);
+    }
+
+    void LimitRot()
+    {
+        Vector3 playerEulerAngles = localTrans.rotation.eulerAngles;
+        playerEulerAngles.y = (playerEulerAngles.y > 180) ? playerEulerAngles.y - 360 : playerEulerAngles.y;
+        playerEulerAngles.y = Mathf.Clamp(playerEulerAngles.y, minYRot, maxYRot);
+        localTrans.rotation = Quaternion.Euler(playerEulerAngles);
+    }
+
+    void WASDMove()
+    {
         Vector3 pos = transform.position;
         if (Input.GetKey("w"))
         {
@@ -35,13 +65,5 @@ public class Playermovements : MonoBehaviour
             pos.x += speed * Time.deltaTime;
         }
         transform.position = pos;
-        //turn.x += Input.GetAxis("Mouse X");
-        //turn.y += Input.GetAxis("Mouse Y");
-        //transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
-    }
-
-    void MoveForward()
-    {
-        gameObject.transform.Translate(Vector3.forward * mySpeed * Time.deltaTime, Space.World);
     }
 }
